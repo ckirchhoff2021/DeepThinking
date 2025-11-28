cbd30c585299a12189dd6001d2db3f1f4b752e70
ckirchhoff <ckirchhoff2019@gmail.com>
2025-11-28 17:36:47 +0800

    Add functions for current time and weather

--- agents/functions.py
+++ agents/functions.py
@@ -1,1 +1,41 @@
 
+
+from datetime import datetime
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_current_time",
+            "description": "当你想知道现在的时间时非常有用。",
+            "parameters": {}  
+        }
+    },  
+    {
+        "type": "function",
+        "function": {
+            "name": "get_current_weather",
+            "description": "当你想查询指定城市的天气时非常有用。",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "城市或县区，比如北京市、杭州市、余杭区等。"
+                    }
+                },
+                "required": [
+                    "location"
+                ]
+            }
+        }
+    }
+]
+
+def get_current_weather(location):
+    return f"{location}今天是雨天。 "
+
+def get_current_time():
+    current_datetime = datetime.now()
+    formatted_time = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+    return f"当前时间：{formatted_time}。"
